82d19d7c702b7fafb4958c9f6e2e39fa21a15714
Anthony Pierce <Ant.pierce24@gmail.com>
2022-04-26 21:25:28 -0400

    Update Graphics_Final.java
    YYYYYYYYYYYYYYYYYYEEEEEEEEEEEEEEEEEAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHH

--- src/Graphics_Final.java
+++ src/Graphics_Final.java
@@ -27,12 +27,9 @@ public class Graphics_Final extends Applet {
         setLayout(new BorderLayout());
         add(cv, BorderLayout.CENTER);
 
-        Panel menu = new Panel();
-
         SimpleUniverse su = new SimpleUniverse(cv, 2);
         su.getViewingPlatform().setNominalViewingTransform();
-        BranchGroup bg = createSceneGraph(su.getViewingPlatform().
-                getMultiTransformGroup().getTransformGroup(0));
+        BranchGroup bg = createSceneGraph(su.getViewingPlatform().getMultiTransformGroup().getTransformGroup(0));
         bg.compile();
         su.addBranchGraph(bg);
     }
@@ -40,10 +37,12 @@ public class Graphics_Final extends Applet {
     Appearance ap;
     Appearance bb;
     private BranchGroup createSceneGraph(TransformGroup vtg) {
+        //Branch Group
         BranchGroup root = new BranchGroup();
         BoundingSphere bounds = new BoundingSphere();
         bounds.setRadius(2);
-        //object
+
+        //Appearance
         ap = new Appearance();
         bb = new Appearance();
         Material grey = new Material();
@@ -51,30 +50,77 @@ public class Graphics_Final extends Applet {
         grey.setDiffuseColor(.5f,.5f,.5f);
         mat.setDiffuseColor(100,0,0);
         ap.setMaterial(mat);
-
+        bb.setMaterial(grey);
+        
         Transform3D tr = new Transform3D();
-        tr.setScale(0.25);
+        Transform3D tr1 = new Transform3D();
+        Transform3D tr2 = new Transform3D();
+
+        tr.setScale(1);
+        tr1.setScale(0.25);
+        tr2.setScale(1);
+
+        Vector3f vec = new Vector3f(0f,1f, .0f);
+        Vector3f vec1 = new Vector3f(0f,0f, .0f);
+        Vector3f vec2 = new Vector3f(0f, -1f, .0f);
+
+        tr.setTranslation(vec);
+        tr1.setTranslation(vec1);
+        tr2.setTranslation(vec2);
 
         TransformGroup tg = new TransformGroup(tr);
+        TransformGroup tg1 = new TransformGroup(tr1);
+        TransformGroup tg2 = new TransformGroup(tr2);
+
+        tg.setTransform(tr);
+        tg1.setTransform(tr1);
+        tg2.setTransform(tr2);
+
         tg.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
         tg.setCapability(TransformGroup.ALLOW_TRANSFORM_READ);
-        Sphere shape = new Sphere(0.5f, Sphere.GENERATE_NORMALS, 150, ap);
-        tg.addChild(shape);
-        bb.setMaterial(grey);
-        Cylinder cy = new Cylinder(.1f,1,bb);
-        tg.setTransform(tr);
-        tg.addChild(cy);
 
-        root.addChild(tg);
+        tg1.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+        tg1.setCapability(TransformGroup.ALLOW_TRANSFORM_READ);
+
+        tg2.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+        tg2.setCapability(TransformGroup.ALLOW_TRANSFORM_READ);
+
+        //Shapes
+        Sphere shape = new Sphere(0.5f, Sphere.GENERATE_NORMALS, 150, ap);
+        Cylinder cy = new Cylinder(.1f, Cylinder.GENERATE_NORMALS, bb);
+        Sphere shape1 = new Sphere(0.5f, Sphere.GENERATE_NORMALS, 150, ap);
+
+        cy.addChild(tg);
+        cy.addChild(tg2);
+
+        tg.addChild(shape);     
+        tg1.addChild(cy);
+        tg2.addChild(shape1);
+        
+        tg.setTransform(tr);
+        tg1.setTransform(tr1);
+        tg2.setTransform(tr2);
+
+        //root.addChild(tg);
+        root.addChild(tg1);
+        //root.addChild(tg2);
 
         //view rotator
-        MouseRotate rotater = new MouseRotate(tg);
+        /*MouseRotate rotater = new MouseRotate(tg);
         rotater.setSchedulingBounds(bounds);
         tg.addChild(rotater);
 
         MouseTranslate translator = new MouseTranslate(tg);
         translator.setSchedulingBounds(bounds);
-        tg.addChild(translator);
+        tg.addChild(translator);*/
+
+        MouseRotate rotater1 = new MouseRotate(tg1);
+        rotater1.setSchedulingBounds(bounds);
+        tg1.addChild(rotater1);
+
+        MouseTranslate translator1 = new MouseTranslate(tg1);
+        translator1.setSchedulingBounds(bounds);
+        tg1.addChild(translator1);
 
 
         // background and lights
@@ -90,9 +136,6 @@ public class Graphics_Final extends Applet {
         dLight2 = new DirectionalLight(new Color3f(Color.WHITE), new Vector3f(20,-1,-5f));
         dLight.setInfluencingBounds(bounds);
         root.addChild(dLight);
-
-
-
 
         return root;
     }
